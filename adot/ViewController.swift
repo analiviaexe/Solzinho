09c9628e74221a423c0e94a12fa3235ee8642f34
Ana Livia <analivia.exe@gmail.com>
2022-02-16 14:16:20 -0300

    projeto solzinho

--- adot/ViewController.swift
+++ adot/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelSol: UILabel!
+    @IBOutlet weak var iconSol: UIImageView!
+    @IBOutlet weak var bgSol: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func mudarTempo(_ sender: Any) {
+        labelSol.text = "Chuvinha"
+        labelSol.textColor = UIColor.black
+        iconSol.image = #imageLiteral(resourceName: "rain")
+        bgSol.image = #imageLiteral(resourceName: "rainBackground")
+    }
+    
 }
 
